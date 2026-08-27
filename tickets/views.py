0b1231f051214a4c5b7e4d710454101438ac6160
Vinicius <vini.r.faria@hotmail.com>
2026-08-27 16:55:54 -0300

    feat(backend): integra api do gemini para resumo automático de chamados

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,27 @@
 from rest_framework import viewsets
 from .models import Ticket
 from .serializers import TicketSerializer
+import google.generativeai as genai
+
+genai.configure(api_key="YOUR_API_KEY")
+model = genai.models.GenerativeModel("gemini-1.5-flash")
+
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+
+    def perform_create(self, serializer):
+        # Pega a descrição que o React enviou no POST
+        descricao = serializer.validated_data.get('description', '')
+        
+        try:
+           
+            prompt = f"Atue como um analista de suporte de TI. Faça um resumo técnico, em apenas uma frase curta, sobre este problema: {descricao}"
+            resposta = model.generate_content(prompt)
+            resumo_gerado = resposta.text.strip()
+        except Exception as e:
+            print(f"Erro ao conectar com a IA: {e}")
+            resumo_gerado = "Não foi possível gerar o resumo automático."
+            
+        serializer.save(ai_summary=resumo_gerado)
